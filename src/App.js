37320efaa7f6646622a966e9ae680c82e9130967
Sumedh Indulkar <cscoder2002@gmail.com>
2022-05-05 09:34:47 +0530

    design of calculator ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useReducer } from "react";
 function App() {
+  const data = [
+    { type: "clear", symbol: "C" },
+    { type: "dummy", symbol: "+/-" },
+    { type: "percentage", symbol: "%" },
+    { type: "divide", symbol: "÷" },
+    { type: "number", symbol: "7" },
+    { type: "number", symbol: "8" },
+    { type: "number", symbol: "9" },
+    { type: "multiply", symbol: "X" },
+    { type: "number", symbol: "4" },
+    { type: "number", symbol: "5" },
+    { type: "number", symbol: "6" },
+    { type: "subtract", symbol: "-" },
+    { type: "number", symbol: "1" },
+    { type: "number", symbol: "2" },
+    { type: "number", symbol: "3" },
+    { type: "add", symbol: "+" },
+  ];
+
+  const initialState = {
+    op1: 1,
+    op2: 2,
+    operator: "+",
+    ans: 0,
+  };
+  const reducer = (state, action) => {
+    switch (action) {
+      case 1:
+        return state + 1;
+      default:
+        throw new Error("Error");
+    }
+  };
+  const [ans, dispatch] = useReducer(reducer, initialState.ans);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="design">
+        <div className="box1 neuro"></div>
+        <div className="box2 neuro"></div>
+        <div className="circle1 neuro"></div>
+        <div className="circle2 neuro"></div>
+        <div className="main neuro">
+          <div className="top">
+            <span>{ans}</span>
+          </div>
+          <div className="bottom">
+            {data &&
+              data.map((item) => {
+                return (
+                  <button className="keys" onClick={() => dispatch(1)}>
+                    {item.symbol}
+                  </button>
+                );
+              })}
+            <button className="keys w-40">O</button>
+            <button className="keys ">.</button>
+            <button className="keys clear">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
